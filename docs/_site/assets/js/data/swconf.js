ca71e355d75b54e13178a57c40a6e8db492eade7
Ashish Ramesh <ashishramesh2003@gmail.com>
2025-05-18 13:36:31 +0530

    initial setup

---
+++ docs/_site/assets/js/data/swconf.js
@@ -0,0 +1,30 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1747555532',resources: [
+      '/gsoc2025-Ashish_Ramesh/assets/css/jekyll-theme-chirpy.css',
+      '/gsoc2025-Ashish_Ramesh/',
+      
+        '/gsoc2025-Ashish_Ramesh/categories/',
+      
+        '/gsoc2025-Ashish_Ramesh/tags/',
+      
+        '/gsoc2025-Ashish_Ramesh/archives/',
+      
+        '/gsoc2025-Ashish_Ramesh/about/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
---
+++ docs/_tabs/about.md
@@ -0,0 +1,10 @@
+---
+# the default layout is 'page'
+icon: fas fa-info-circle
+order: 4
+---
+
+> Hello!
+{: .prompt-tip }
+
+Blog that compiles the activities and tasks completed as part of my Gsoc'25 Project with JdeRobot.
