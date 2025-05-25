e9980b235a3370b29e4e834ac8c82b7ece293355
Ashish Ramesh <ashishramesh2003@gmail.com>
2025-05-25 11:28:07 +0530

    bundle install fix

--- docs/_site/assets/js/data/swconf.js
+++ docs/_site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1748152247',resources: [
+    cacheName: 'chirpy-1748152628',resources: [
       '/gsoc2025-Ashish_Ramesh/assets/css/jekyll-theme-chirpy.css',
       '/gsoc2025-Ashish_Ramesh/',
       
