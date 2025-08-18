bf4827b41a3b5388561ae389d70568fd83e204d2
Ashish Ramesh <ashishramesh2003@gmail.com>
2025-08-18 23:10:20 +0530

    week 9 & 10 blog updated

--- docs/_site/assets/js/data/swconf.js
+++ docs/_site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1755448970',resources: [
+    cacheName: 'chirpy-1755538816',resources: [
       '/gsoc2025-Ashish_Ramesh/assets/css/jekyll-theme-chirpy.css',
       '/gsoc2025-Ashish_Ramesh/',
       
